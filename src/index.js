6786ed6ae956b7982b0a8e7e691d411fe9c5e9c4
ajb85 <19802883+ajb85@users.noreply.github.com>
2020-02-20 14:09:09 -0500

    Adds new private routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import PublicRoutes from './components/Routes/Public.js';
+import PrivateRoutes from './components/Routes/Private.js';
 
 // Context
 import Providers from './Providers/';
@@ -16,6 +17,7 @@ ReactDOM.render(
   <Providers>
     <Router history={history}>
       <PublicRoutes />
+      <PrivateRoutes />
     </Router>
   </Providers>,
   document.getElementById('root')
