4ecdb1cfcf4429fbb21f9b84bdf790146d5b3b67
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-02-22 19:47:26 -0500

    Calculates the size of the image based off the window and ensures image never goes off screen (vert or hori)

--- src/components/MainImage/MainImage.js
+++ src/components/MainImage/MainImage.js
@@ -1,14 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './styles.module.scss';
 
-function MainImage({ image }) {
+function MainImage({ image, showMenuState }) {
+  const [showMenu] = showMenuState;
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const resizeWindow = () => {
+      const offset = showMenu
+        ? window.innerWidth * 0.2 <= 300
+          ? 300
+          : window.innerWidth * 0.2
+        : 0;
+      setWindowSize({
+        width: Math.round(window.innerWidth - offset),
+        height: window.innerHeight
+      });
+    };
+
+    window.addEventListener('resize', resizeWindow);
+
+    return () => window.removeEventListener('resize', resizeWindow);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (dimensions.width && dimensions.height) {
+      const ratio = dimensions.width / dimensions.height;
+      const proposedHeight = windowSize.width / ratio;
+
+      setImageSize(
+        proposedHeight >= windowSize.height
+          ? { width: windowSize.height * ratio, height: windowSize.height }
+          : { width: windowSize.width, height: proposedHeight }
+      );
+    }
+  }, [dimensions, windowSize]);
+
   if (!image) {
     return <p>Loading...</p>;
   }
+
+  const loadDimensions = ({ target }) => {
+    if (dimensions.width === 0 && dimensions.height === 0) {
+      setDimensions({ width: target.offsetWidth, height: target.offsetHeight });
+    }
+  };
+
   return (
     <div className={styles.MainImage}>
-      <img src={image.src} alt={image.alt} />
+      <img
+        style={dimensions.width && dimensions.height ? imageSize : null}
+        onLoad={e => loadDimensions(e)}
+        src={image.src}
+        alt={image.alt}
+      />
     </div>
   );
 }
