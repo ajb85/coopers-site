7d1bec37ae648d6b0c753b420db56e148bb5f6f2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-02-22 19:46:30 -0500

    Updates images data structure to an ordered map

--- src/Providers/Images.js
+++ src/Providers/Images.js
@@ -1,29 +1,24 @@
 import React, { useState, useEffect, createContext } from 'react';
 
 import realmBE from 'axios.js';
+import OrderedMap from 'js/OrderedMap.js';
 
 export const ImagesContext = createContext();
 
 export function Images(props) {
-  const [images, setImages] = useState({});
+  const [images, setImages] = useState(new OrderedMap());
   const [fetched, setFetched] = useState(false);
+  console.log(images.storage);
 
   useEffect(() => {
     if (!fetched) {
       setFetched(true);
       realmBE
         .get('/images/all')
-        .then(({ data }) =>
-          setImages(
-            data.reduce((acc, img) => {
-              acc[img.id] = img;
-              return acc;
-            }, {})
-          )
-        )
+        .then(({ data }) => setImages(new OrderedMap(data)))
         .catch(err => console.log('ERROR FETCHING IMAGES: ', err));
     }
-  }, [images, setImages]);
+  }, [images, setImages, fetched]);
 
   const { Provider } = ImagesContext;
 
