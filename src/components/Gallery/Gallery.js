b27a806a5f2029811142ea781d4a4520c29602f8
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-02-22 21:17:07 -0500

    Updates to use simpler images context logic

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 
 import MainImage from '../MainImage/';
 import SideMenu from '../SideMenu/';
@@ -8,28 +8,17 @@ import { ImagesContext } from 'Providers/Images.js';
 import styles from './styles.module.scss';
 
 function Gallery(props) {
-  const { images } = useContext(ImagesContext);
-  const activeImageState = useState(null);
-  const [active] = activeImageState;
+  const { image } = useContext(ImagesContext);
   const showMenuState = useState(true);
 
-  useEffect(() => {
-    if (activeImageState[0] === null && images.length) {
-      activeImageState[1](images.lastID());
-    }
-  }, [activeImageState]);
-
-  if (active === null) {
+  if (!image) {
     return <div>Loading...</div>;
   }
+
   return (
     <div className={styles.Gallery}>
-      <MainImage image={images.get(active)} showMenuState={showMenuState} />
-      <SideMenu
-        activeImageState={activeImageState}
-        showMenuState={showMenuState}
-        images={images}
-      />
+      <MainImage image={image} showMenuState={showMenuState} />
+      <SideMenu active={image.id} showMenuState={showMenuState} />
     </div>
   );
 }
