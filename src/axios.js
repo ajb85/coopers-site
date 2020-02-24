a9496c93a510c116d0622a1d321c767f464d687c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-02-23 20:49:06 -0500

    Uses .env var instead of hardcode value

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('token');
-
+console.log('process.env.REACT_APP_BASEURL: ', process.env.REACT_APP_BASEURL);
 export default axios.create({
-  baseURL: 'http://localhost:4500',
+  baseURL: process.env.REACT_APP_BASEURL,
   headers: {
     authorization: token
   }
