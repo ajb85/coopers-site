e5ea605aae3a172a8d92f7744392e3c6c9856f29
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-02-23 21:01:07 -0500

    Removes log

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const token = localStorage.getItem('token');
-console.log('process.env.REACT_APP_BASEURL: ', process.env.REACT_APP_BASEURL);
+
 export default axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
   headers: {
