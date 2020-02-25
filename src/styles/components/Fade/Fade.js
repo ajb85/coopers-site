e89f8a4119a32a32cd64dd53453f9381d4e036c5
ajb85 <19802883+ajb85@users.noreply.github.com>
2020-02-24 19:37:38 -0500

    Animates bottom text when changing images and adds :hover to override gradient

--- src/styles/components/Fade/Fade.js
+++ src/styles/components/Fade/Fade.js
@@ -2,18 +2,38 @@ import styled, { keyframes } from 'styled-components';
 
 const fadeIn = keyframes`
     from {
-        opacity: 0
+      opacity: 0
     } to {
-        opacity: 1
+      opacity: 1
     }
 `;
 
 const fadeOut = keyframes`
  from {
-        opacity: 1
-    } to {
-        opacity: 0
-    }`;
+    opacity: 1
+  } to {
+    opacity: 0
+  }`;
+
+const hide = keyframes`
+  from {
+    opacity: 1;
+    bottom:0px;
+  } to {
+    opacity: 0;
+    bottom: -75px;
+  }
+`;
+
+const emerge = keyframes`
+  from {
+    opacity: 0;
+    bottom: -75px;
+  } to {
+    opacity: 1;
+    bottom: 0px;
+  }
+`;
 
 export default styled.div`
   width: 80vw;
@@ -41,16 +61,51 @@ export default styled.div`
     position: absolute;
     bottom: 0px;
     margin: 0 auto;
-    padding: 20px;
+    padding: 30px 20px;
     width: 100%;
+    z-index: 1;
 
-    background: rgb(255, 255, 255);
+    opacity: ${({ direction }) => {
+      return direction === 'in' ? 0 : 1;
+    }};
+
+    animation: ${({ direction }) =>
+        direction === 'in' ? emerge : direction === 'out' ? hide : null}
+      0.5s linear
+      ${({ direction }) =>
+        direction === 'in' ? '0.5s' : direction === 'out' ? '0s' : null}
+      1;
+
+    background: rgb(255, 255, 255, 1);
     background: linear-gradient(
       0deg,
       rgba(255, 255, 255, 0.75) 0%,
       rgba(255, 255, 255, 0.6) 50%,
       rgba(255, 255, 255, 0) 100%
     );
+
+    &::before {
+      position: absolute;
+      content: '';
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: -1;
+
+      transition: opacity 0.5s linear;
+      opacity: 0;
+
+      background: linear-gradient(
+        0deg,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(255, 255, 255, 1) 100%
+      );
+    }
+
+    &:hover::before {
+      opacity: 1;
+    }
 
     p {
       margin: 10px 0;
