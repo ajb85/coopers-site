76c837dd99e69f08ebdba4e0b2a7b5fd9d9d80b2
ajb85 <19802883+ajb85@users.noreply.github.com>
2020-02-24 19:55:59 -0500

    Lazy Loads the side menu

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,2 +1,20 @@
-import $ from './SideMenu.js';
-export default $;
+import React from 'react';
+import LazyLoad from 'LazyLoad/';
+
+export default function Lazy(props) {
+  return (
+    <LazyLoad
+      path='components/SideMenu/SideMenu.js'
+      fallback={<Loading />}
+      {...props}
+    />
+  );
+}
+
+function Loading(props) {
+  return (
+    <div>
+      <p>Fetching the good boy...</p>
+    </div>
+  );
+}
